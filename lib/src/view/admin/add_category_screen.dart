649b1f5484d0bb28a58eb767a015096235bda8b7
junayedahamed <junayedahamed660@gmail.com>
2025-04-11 00:50:16 +0600

    add category screen designed

--- lib/src/view/admin/add_category_screen.dart
+++ lib/src/view/admin/add_category_screen.dart
@@ -19,9 +19,9 @@ class _AddCategoryScreenState extends State<AddCategoryScreen> {
   @override
   void initState() {
     super.initState();
-    _nameConroller = TextEditingController(text: widget.category!.name);
+    _nameConroller = TextEditingController(text: widget.category?.name ?? "");
     _descriptionController = TextEditingController(
-      text: widget.category!.description,
+      text: widget.category?.description ?? '',
     );
   }
 
@@ -87,8 +87,156 @@ class _AddCategoryScreenState extends State<AddCategoryScreen> {
     }
   }
 
+  Future<bool> _onWillPop() async {
+    if (_nameConroller.text.isNotEmpty ||
+        _descriptionController.text.isNotEmpty) {
+      return await showDialog<bool>(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text("Discard Changes"),
+                content: Text("Are you  sure you want to discard changes?"),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context, false);
+                    },
+                    child: Text("Cancel"),
+                  ),
+
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context, true);
+                    },
+                    child: Text("Confrim"),
+                  ),
+                ],
+              );
+            },
+          ) ??
+          false;
+    }
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(backgroundColor: AppTheme.backGroundColor));
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppTheme.backGroundColor,
+          title: Text(
+            widget.category != null ? "Edit Category" : "Add Category",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Form(
+              key: _formkey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Category Details",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: AppTheme.textPrimaryColor,
+                    ),
+                  ),
+
+                  SizedBox(height: 8),
+
+                  Text(
+                    "Add New Category for organizing Quiz",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: AppTheme.textSecondaryColor,
+                    ),
+                  ),
+                  SizedBox(height: 24),
+                  TextFormField(
+                    controller: _nameConroller,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      contentPadding: EdgeInsets.symmetric(vertical: 20),
+                      labelText: "Category Name",
+
+                      hintText: "Enter Category name",
+
+                      prefixIcon: Icon(
+                        Icons.category_rounded,
+                        color: AppTheme.primaryColor,
+                      ),
+                    ),
+
+                    validator:
+                        (value) =>
+                            value!.isEmpty ? "Enter Category name" : null,
+                    textInputAction: TextInputAction.next,
+                  ),
+
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: _descriptionController,
+                    decoration: InputDecoration(
+                      labelText: "Category Description",
+                      hintText: "Enter Category  Description",
+                      fillColor: Colors.white,
+                      prefixIcon: Icon(
+                        Icons.description_rounded,
+                        color: AppTheme.primaryColor,
+                      ),
+
+                      alignLabelWithHint: true,
+                    ),
+                    maxLines: 3,
+
+                    validator:
+                        (value) =>
+                            value!.isEmpty ? "Enter  Description name" : null,
+                    textInputAction: TextInputAction.next,
+                  ),
+
+                  SizedBox(height: 32),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+
+                    child: ElevatedButton(
+                      onPressed: _isLoading ? null : _saveCategory,
+                      child:
+                          _isLoading
+                              ? SizedBox(
+                                height: 20,
+                                width: 20,
+                                child: CircularProgressIndicator(
+                                  valueColor: AlwaysStoppedAnimation<Color>(
+                                    Colors.white,
+                                  ),
+                                  strokeWidth: 2,
+                                ),
+                              )
+                              : Text(
+                                widget.category != null
+                                    ? "Update Category"
+                                    : "Add Category",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
