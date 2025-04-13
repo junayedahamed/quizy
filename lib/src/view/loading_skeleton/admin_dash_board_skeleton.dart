5c2c8c863053b0f3102103b5d87132c7cc28bad2
AbulHasnatHridoy <hasnathridoy009@gmail.com>
2025-04-13 23:35:40 +0600

    Test

--- lib/src/view/loading_skeleton/admin_dash_board_skeleton.dart
+++ lib/src/view/loading_skeleton/admin_dash_board_skeleton.dart
@@ -10,7 +10,7 @@ class AdminDashBoardSkeleton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
+   // double width = MediaQuery.of(context).size.width;
     return Skeletonizer(
       child: Scaffold(
         appBar: AppBar(
